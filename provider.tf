02336a63472feb4b82a9401a91aeb7fc900a1632
gmstcl <as.gmstcl@gmail.com>
2025-03-15 11:18:41 +0900

    init

--- provider.tf
+++ provider.tf
@@ -1,15 +1,15 @@
 terraform {
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "~> 5.0"
-  #   }
-  # }
-    backend "s3" {
-        bucket = "demo-running-tfstate"
-        key    = "terraform.tfstate"
-        region = "ap-northeast-2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
+  backend "s3" {
+      bucket = "demo-running-tfstate"
+      key    = "terraform.tfstate"
+      region = "ap-northeast-2"
+  }
 }
 
 # Configure the AWS Provider

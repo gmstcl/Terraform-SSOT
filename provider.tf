27cf340accf47344560df014f30a45183ed4f0e7
gmstcl <as.gmstcl@gmail.com>
2025-03-15 10:29:38 +0900

    init

--- provider.tf
+++ provider.tf
@@ -15,6 +15,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-northeast-2"
-  access_key = "${env.AWS_ACCESS_KEY_ID}"
-  secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
 }
